a37f63f4cf24bc277297829d32c0ae882236c498
JohnFreddyTutistar <johnfre.157@gmail.com>
2024-07-30 17:47:30 -0500

    creando componentes home y about e implementado rutas basicas

---
+++ src/app/shared/pages/about-pages/about-pages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-pages',
+  templateUrl: './about-pages.component.html',
+  styleUrls: ['./about-pages.component.scss']
+})
+export class AboutPagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
